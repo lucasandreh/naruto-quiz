eeae53aeb2262fa1e232aeb21adf720ab245c62e
LLOFYY <30734697+llofyy@users.noreply.github.com>
2021-01-25 20:30:45 -0300

    Adicionando head ao projeto

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -5,13 +5,15 @@ function IndexPage() {
   return (
     <div>
       <Head>
-        <title>Naruto Quiz</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://naruto-quiz.llofyy.vercel.app/" />
-        <meta property="og:title" content="NARUTO QUIZ" />
-        <meta property="og:description" content="Projeto realizado durante a Imersão React v2." />
-        <meta property="og:image" content={db.bg}/>
+          <head>
+            <title>Naruto Quiz</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://naruto-quiz.llofyy.vercel.app/" />
+            <meta property="og:title" content="NARUTO QUIZ" />
+            <meta property="og:description" content="Projeto realizado durante a Imersão React v2." />
+            <meta property="og:image" content={db.bg}/>
+          </head>
       </Head>
     </div>
   )
