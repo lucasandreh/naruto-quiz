86389229a965fcf4cbe964565ddad99afab28ec9
LLOFYY <30734697+llofyy@users.noreply.github.com>
2021-01-25 20:24:07 -0300

    Arrumando o head

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -7,6 +7,8 @@ function IndexPage() {
       <Head>
         <title>Naruto Quiz</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="title" content="NARUTO QUIZ" />
+        <meta name="description" content="Projeto realizado durante a Imersão React v2." />
         <meta property="og:image" content={db.bg}/>
       </Head>
     </div>
